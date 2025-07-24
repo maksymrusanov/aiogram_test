018def5b2d8126744120914f4ee33e970b3f682c
maksym <1maksymrusanov1@gmail.com>
2025-07-24 16:16:23 +0100

    add keyboard interaption("second try")

---
+++ app/handlers.py
@@ -0,0 +1,53 @@
+from aiogram import F, Router
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+# импортировали этот модуль
+import app.keyboards as kb
+# роутер чтобы не было ошибок при рутинге(меняем все dp на router)
+router = Router()
+
+
+@router.message(CommandStart())
+# обработка команды старт
+async def cmd_start(message: Message):
+    await message.answer('hello', reply_markup=kb.main)
+
+
+@router.message(Command('id'))
+# возврат id по запросу /id с методом reply(ответит на сообщение)
+async def cmd_start(message: Message):
+    await message.reply(f'your ID is: {message.from_user.id}')
+
+
+@router.message(Command('help'))
+# обработка команды help,ответ будет просто сообщением
+async def get_help(message: Message):
+    await message.answer('this is /help command')
+
+
+@router.message(F.text == 'good?')
+# F-magic filter(пока что хз что это)
+# сравниваем текст пользователя для отлова сообщения,если есть-выводит ответ
+async def how_r_u(message: Message):
+    await message.answer('all good?')
+
+
+@router.message(F.photo)
+async def get_photo(message: Message):
+    # отлов картинки и ответ с id этой картинки([-1]это указание качества)
+    await message.answer(f'id of picture:{message.photo[-1].file_id}')
+
+
+@router.message(Command('photo'))
+# пример запроса: /photo
+# возврат картинки по id(таким образом можно отправлять картинки без скачивания)
+async def get_photo(message: Message):
+    await message.answer_photo(photo='AgACAgIAAxkBAAMUaII8hvciNOkT3bxQIEpBvY9P7N0AAuP1MRv1GxFIWr3hsAdiVw8BAAMCAAN5AAM2BA', caption='this is black picture')
+
+
+@router.message(Command('photo_1'))
+# пример запроса: /photo_1
+# возврат картинки по ссылке(таким образом можно отправлять картинки без скачивания)
+async def get_photo(message: Message):
+    await message.answer_photo(photo='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%2Fid%2FOIP.n-_WWS8nyodVAXv4HiKxagHaEW%3Fpid%3DApi&f=1&ipt=45f62af95bb5c38aa69246a4697db38f92008b0091683c120584fcdc9aa4ec56&ipo=images',
+                               caption='this is another picture')
---
+++ app/keyboards.py
@@ -0,0 +1,7 @@
+# создаём файл взаимодействия с клавой
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+# создали кнопки(2 ряда,в первом одна кнопка,во втором-2)
+main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='button1')],
+    # это один ряд
+    [KeyboardButton(text='button2'), KeyboardButton(text='button3')]])
