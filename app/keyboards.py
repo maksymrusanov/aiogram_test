91a7433560aaa8abfa67faa311d8692b2fb98e78
maksym <1maksymrusanov1@gmail.com>
2025-07-24 16:31:38 +0100

    add req file

--- app/keyboards.py
+++ app/keyboards.py
@@ -4,4 +4,6 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='button1')],
     # это один ряд
-    [KeyboardButton(text='button2'), KeyboardButton(text='button3')]])
+    [KeyboardButton(text='button2'), KeyboardButton(text='button3')]],
+    resize_keyboard=True,
+    input_field_placeholder='choose option')
